5d8c0c83340c3c71dac007ccd3c09c531f02dd56
pazho <kochi.bk@googlemail.com>
2025-10-30 19:25:59 -0400

    C:first funktions def

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        
+    return file_contents
+       
+def main():
+    content = get_book_text("books/frankenstein.txt")
+    print(content)
+
+main()
