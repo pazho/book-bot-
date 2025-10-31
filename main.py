01d9e0feaad675325e4f5aca63fa7bbabe361aad
pazho <kochi.bk@googlemail.com>
2025-10-31 14:13:58 -0400

    F: almost done

--- main.py
+++ main.py
@@ -8,11 +8,22 @@ from stats import count_words
 
 from stats import word_counter
 
+from stats import sorting
+
 def main():
     content = get_book_text("books/frankenstein.txt")
     chars = word_counter(content)
+    sorted_chars = sorting(chars)
+    file_path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
     print(count_words(content))
-    print(chars)
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
 
 
 main()
